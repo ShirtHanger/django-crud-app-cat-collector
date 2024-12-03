5118a4c83c79e41d50103826ec94a8a8996f02dc
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-12-03 14:31:02 -0600

    home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [ # Routes will be added here
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # route for cats index
+  path('cats/', views.cat_index, name='cat-index'),
 ]
 
 """ 
