92eead5868e803ea692f271d0eb97a1ba7fefa55
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-12-04 12:26:52 -0600

    Did module 6

--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 # Import HttpResponse to send text-based responses
-# Obeslete
+# Obeslete when you make a proper home page
 from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
-    # Send a simple HTML response
+    # Returns main_app/templates/home.html
     return render(request, 'home.html')
 
 # Now let's define an about page
@@ -35,4 +35,5 @@ cats = [
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    # Turns it into a dictionary with a single key, 'cats'
     return render(request, 'cats/index.html', {'cats': cats})
